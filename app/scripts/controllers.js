2eb42c2cc1a15500a17c99e2ec49dd7706b601c6
Dan Cusher <dan@dancusher.com>
2016-04-02 03:11:15 +0000

    Initial commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+var app = angular.module('friendsApp')
+
+	.controller('ContactController', ['$scope', function($scope) {
+		
+	}])
+	
+	.controller('HeaderController', ['$scope', '$state', function($scope, $state) {
+		$scope.foo = 'foo';
+		$scope.bar = $state.current.name;
+		$scope.$state = $state;
+	}])
+
+;
---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+'use strict';
+var app = angular.module('friendsApp',['ui.router'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+                    // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                        controller : 'HeaderController',
+                    },
+                    'content': {
+                        template : '<h1>Index - To be Completed</h1>',
+                        controller  : 'IndexController',
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html'
+                    }
+                }
+            })
+                    // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        template: '<h1>About Us - To be Completed</h1>'
+                   }
+                }
+            })
+                    // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'
+                     }
+                }
+            });
+            
+            $urlRouterProvider.otherwise('/');
+    })
+
+;
