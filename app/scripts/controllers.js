46d8059564797dad8fdf0a0a7209731a5f6f1f9c
Dan Cusher <dan@dancusher.com>
2016-05-07 14:39:51 +0000

    Added board and committee lists to about page

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -16,7 +16,103 @@ var app = angular.module('friendsApp')
 	}])
 
 	.controller('AboutController', ['$scope', function($scope) {
-
+		$scope.leadership = {
+			board: [
+				{
+					position: "President",
+					name: "Dan Cusher"
+				},
+				{
+					position: "Vice President",
+					name: "Gail Wixon"
+				},
+				{
+					position: "Secretary",
+					name: "Elinor Tidman"
+				},
+				{
+					position: "Treasurer",
+					name: "Ellen Gougen"
+				},
+				{
+					position: "Director of Down Under",
+					name: "Mary Tulloch"
+				},
+				{
+					position: "Publicity Chair",
+					name: "Michelle Dalal"
+				},
+				{
+					position: "Membership Chair",
+					name: "Vacant"
+				},
+				{
+					position: "At-Large (Co-Treasurer)",
+					name: "Karen Kara’a"
+				},
+				{
+					position: "At-Large",
+					name: "Alberta Dumfree"
+				},
+				{
+					position: "At-Large",
+					name: "Gail Poler"
+				},
+				{
+					position: "At-Large",
+					name: "Aaron Swartz"
+				}
+			],
+			committees: [
+				{
+					name: "Publicity Committee",
+					members: [
+						{
+							position: "Chair",
+							name: "Michelle Dalal"
+						},
+						{
+							position: "Member",
+							name: "Vacant"
+						},
+						{
+							position: "Member",
+							name: "Vacant"
+						}
+					]
+				},
+				{
+					name: "Membership Committee",
+					members : [
+						{
+							position: "Chair",
+							name: "Vacant"
+						},
+						{
+							position: "Member",
+							name: "Vacant"
+						},
+						{
+							position: "Member",
+							name: "Vacant"
+						}
+					]
+				},
+				{
+					name: "Nominating Committee",
+					members: [
+						{
+							position: "Chair",
+							name: "Vacant"
+						},
+						{
+							position: "Member",
+							name: "Vacant"
+						}
+					],
+				},
+			]
+		}
 	}])
 
 ;
