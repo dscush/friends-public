95c9e5f51dc4e668b9fc2f03d0d07105706f4f1d
Dan Cusher <dan@dancusher.com>
2016-06-22 11:09:31 -0400

    Cleaned up JavaScript 'errors' reported by Lint

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,5 +1,6 @@
 'use strict';
-var app = angular.module('friendsApp')
+/*global angular*/
+angular.module('friendsApp')
 
 	.controller('ContactController', ['$scope', '$stateParams', function($scope, $stateParams) {
 		$scope.contactForm = {
@@ -11,115 +12,114 @@ var app = angular.module('friendsApp')
 		$scope.submit = function (contactForm) {
 			//TODO: get server up and running and implement something on server side
 			console.log(contactForm);
-		}
+		};
 	}])
-	
+
 	.controller('HeaderController', ['$scope', '$state', function($scope, $state) {
 		$scope.foo = 'foo';
 		$scope.bar = $state.current.name;
 		$scope.$state = $state;
 	}])
-	
-	.controller('HomeController', ['$scope', function($scope) {
+
+	.controller('HomeController', [function() {
 	}])
 
-	.controller('JoinController', ['$scope', function($scope) {
-		
+	.controller('JoinController', [function() {
 	}])
 
 	.controller('AboutController', ['$scope', function($scope) {
 		$scope.leadership = {
 			board: [
 				{
-					position: "President",
-					name: "Dan Cusher"
+					position: 'President',
+					name: 'Dan Cusher'
 				},
 				{
-					position: "Vice President",
-					name: "Gail Wixon"
+					position: 'Vice President',
+					name: 'Gail Wixon'
 				},
 				{
-					position: "Secretary",
-					name: "Elinor Tidman"
+					position: 'Secretary',
+					name: 'Elinor Tidman'
 				},
 				{
-					position: "Treasurer",
-					name: "Ellen Gougen"
+					position: 'Treasurer',
+					name: 'Ellen Gougen'
 				},
 				{
-					position: "Director of Down Under",
-					name: "Mary Tulloch"
+					position: 'Director of Down Under',
+					name: 'Mary Tulloch'
 				},
 				{
-					position: "Publicity Chair",
-					name: "Michelle Dalal"
+					position: 'Publicity Chair',
+					name: 'Michelle Dalal'
 				},
 				{
-					position: "Membership Chair",
-					name: "Marie Berardi"
+					position: 'Membership Chair',
+					name: 'Marie Berardi'
 				},
 				{
-					position: "At-Large (Co-Treasurer)",
-					name: "Karen Kara’a"
+					position: 'At-Large (Co-Treasurer)',
+					name: 'Karen Kara’a'
 				},
 				{
-					position: "At-Large",
-					name: "Alberta Dumfree"
+					position: 'At-Large',
+					name: 'Alberta Dumfree'
 				},
 				{
-					position: "At-Large",
-					name: "Gail Poler"
+					position: 'At-Large',
+					name: 'Gail Poler'
 				},
 				{
-					position: "At-Large",
-					name: "Aaron Swartz"
+					position: 'At-Large',
+					name: 'Aaron Swartz'
 				}
 			],
 			committees: [
 				{
-					name: "Publicity Committee",
-					description: "The Publicity Committee's tasks include managing all communication with the public (advertisements, press releases, etc.), assisting the Membership Committee in communication with the membership, and discussing how the Friends can assist the library's publicity/marketing needs with the Library Director and/or staff liason. The committee is appointed by the President and consists of at least two members, including a Chair who serves on the Board of Directors.",
+					name: 'Publicity Committee',
+					description: 'The Publicity Committee\'s tasks include managing all communication with the public (advertisements, press releases, etc.), assisting the Membership Committee in communication with the membership, and discussing how the Friends can assist the library\'s publicity/marketing needs with the Library Director and/or staff liason. The committee is appointed by the President and consists of at least two members, including a Chair who serves on the Board of Directors.',
 					members: [
 						{
-							position: "Chair",
-							name: "Michelle Dalal"
+							position: 'Chair',
+							name: 'Michelle Dalal'
 						},
 						{
-							position: "Member",
-							name: "Vacant"
+							position: 'Member',
+							name: 'Vacant'
 						}
 					]
 				},
 				{
-					name: "Membership Committee",
-					description: "The Membership Committee's tasks include maintaining the membership list, ensuring that renewal notices are mailed, communicating with the membership via Constant Contact and Facebook, and recording and acknowledging all membership dues received and ensuring that the Tresurer receives the money. The committee is appointed by the President and consists of at least two members, including a Chair who serves on the Board of Directors.",
-					members : [
+					name: 'Membership Committee',
+					description: 'The Membership Committee\'s tasks include maintaining the membership list, ensuring that renewal notices are mailed, communicating with the membership via Constant Contact and Facebook, and recording and acknowledging all membership dues received and ensuring that the Tresurer receives the money. The committee is appointed by the President and consists of at least two members, including a Chair who serves on the Board of Directors.',
+					members: [
 						{
-							position: "Chair",
-							name: "Vacant"
+							position: 'Chair',
+							name: 'Vacant'
 						},
 						{
-							position: "Member",
-							name: "Vacant"
+							position: 'Member',
+							name: 'Vacant'
 						}
 					]
 				},
 				{
-					name: "Nominating Committee",
-					description: "The Nominating Committee's task is to present at least one nomination for each office to the general membership at the Annual Meeting. The committee is appointed by the Board of Directors and consists of at least two people.",
+					name: 'Nominating Committee',
+					description: 'The Nominating Committee\'s task is to present at least one nomination for each office to the general membership at the Annual Meeting. The committee is appointed by the Board of Directors and consists of at least two people.',
 					members: [
 						{
-							position: "Chair",
-							name: "Vacant"
+							position: 'Chair',
+							name: 'Vacant'
 						},
 						{
-							position: "Member",
-							name: "Vacant"
+							position: 'Member',
+							name: 'Vacant'
 						}
-					],
-				},
+					]
+				}
 			]
-		}
+		};
 	}])
 
 ;
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,85 +1,85 @@
 'use strict';
-var app = angular.module('friendsApp',['ui.router', 'ui.router.title'])
+/*global angular*/
+angular.module('friendsApp', ['ui.router', 'ui.router.title'])
 .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
                     // route for the home page
             .state('app', {
-                url:'/',
+                url: '/',
                 views: {
                     'header': {
-                        templateUrl : 'views/header.html',
-                        controller : 'HeaderController',
+                        templateUrl: 'views/header.html',
+                        controller: 'HeaderController'
                     },
                     'content': {
-                        templateUrl : 'views/home.html',
-                        controller  : 'HomeController',
+                        templateUrl: 'views/home.html',
+                        controller: 'HomeController'
                     },
                     'footer': {
-                        templateUrl : 'views/footer.html'
-                    },
+                        templateUrl: 'views/footer.html'
+                    }
                 },
                 resolve: {
-                    $title: function() { return 'Home';}
-                },
+                    $title: function() { return 'Home'; }
+                }
             })
                     // route for the about page
             .state('app.about', {
-                url:'about',
+                url: 'about',
                 views: {
                     'content@': {
-                        templateUrl : 'views/about.html',
-                        controller  : 'AboutController'
+                        templateUrl: 'views/about.html',
+                        controller: 'AboutController'
                    }
                 },
                 resolve: {
-                    $title: function() { return 'About Us';}
-                },
+                    $title: function() { return 'About Us'; }
+                }
             })
                     // route for the contact page
             .state('app.contact', {
-                url:'contact',
+                url: 'contact',
                 params: {
-                    name: "",
-                    email: "",
-                    subject: "",
-                    message: "",
+                    name: '',
+                    email: '',
+                    subject: '',
+                    message: ''
                 },
                 views: {
                     'content@': {
-                        templateUrl : 'views/contact.html',
-                        controller  : 'ContactController'
+                        templateUrl: 'views/contact.html',
+                        controller: 'ContactController'
                      }
                 },
                 resolve: {
-                    $title: function() { return 'Contact Us';}
-                },
+                    $title: function() { return 'Contact Us'; }
+                }
             })
                     // route for the join page
             .state('app.join', {
-                url:'join',
+                url: 'join',
                 views: {
                     'content@': {
-                        templateUrl : 'views/join.html',
-                        controller  : 'JoinController'
+                        templateUrl: 'views/join.html',
+                        controller: 'JoinController'
                      }
                 },
                 resolve: {
-                    $title: function() { return 'Join The Friends';}
-                },
+                    $title: function() { return 'Join The Friends'; }
+                }
             })
                     // route for the down under page
             .state('app.downunder', {
-                url:'downunder',
+                url: 'downunder',
                 views: {
                     'content@': {
-                        templateUrl : 'views/downunder.html'
+                        templateUrl: 'views/downunder.html'
                      }
                 },
                 resolve: {
-                    $title: function() { return 'Down Under Bookstore';}
-                },
+                    $title: function() { return 'Down Under Bookstore'; }
+                }
             });
-            
             $urlRouterProvider.otherwise('/');
     })
 
